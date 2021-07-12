c3ab6cef604b4ed35fc7cb28a4f936571cd2d227
Tarcaxoxide <loganer@vivaldi.net>
2021-07-12 16:34:47 -0400

    added L10

---
+++ Scripts/Levels/L8.gd
@@ -0,0 +1,12 @@
+extends "res://Scripts/Level.gd"
+
+export var Data:Dictionary = TemplateData.duplicate(true)
+
+func _ready():
+	Data["Level"]["Current Scene"]="L8"
+	Data["Level"]["Options"].append(_NewOption("Continue","res://Scenes/Levels/L9.tscn",$Selection))
+	_prep_ready(Data)
+	$Selection.visible=true
+
+func _process(delta):
+	P_process(delta,Data)
