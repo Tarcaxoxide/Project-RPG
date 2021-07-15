fc42f57bc86788f822adc5a110491ac3777b06dc
Tarcaxoxide <loganer@vivaldi.net>
2021-07-15 09:33:02 -0400

    0-6 scripts adjusted.

--- Games/Ravager/Scenes/3.gd
+++ Games/Ravager/Scenes/3.gd
@@ -3,9 +3,10 @@ extends "res://Assets/Level.gd"
 export var Data:Dictionary = TemplateData.duplicate(true)
 
 func _ready():
-	$Dialog_Output.text=""
-	Data["Level"]["Current Scene"]="0"
-	#Data["Level"]["Options"].append(_NewOption("Continue.","res://Games/Ravager/Scenes/1.tscn",$Selection))
+	$Dialog_Output.text="It is dark. You are coiled tightly around yourself. From somewhere beyond, you can hear a singing."
+	Data["Level"]["Current Scene"]="3"
+	Data["Level"]["Options"].append(_NewOption("Reach out.","res://Games/Ravager/Scenes/6.tscn",$Selection))
+	Data["Level"]["Options"].append(_NewOption("Listen.","res://Games/Ravager/Scenes/4.tscn",$Selection))
 	_prep_ready(Data)
 
 func _process(delta):
--- Games/Ravager/Scenes/4.gd
+++ Games/Ravager/Scenes/4.gd
@@ -3,9 +3,9 @@ extends "res://Assets/Level.gd"
 export var Data:Dictionary = TemplateData.duplicate(true)
 
 func _ready():
-	$Dialog_Output.text=""
-	Data["Level"]["Current Scene"]="0"
-	#Data["Level"]["Options"].append(_NewOption("Continue.","res://Games/Ravager/Scenes/1.tscn",$Selection))
+	$Dialog_Output.text="The singing continues. it seems familiar, somehow, soothing."
+	Data["Level"]["Current Scene"]="4"
+	Data["Level"]["Options"].append(_NewOption("Continue.","res://Games/Ravager/Scenes/5.tscn",$Selection))
 	_prep_ready(Data)
 
 func _process(delta):
