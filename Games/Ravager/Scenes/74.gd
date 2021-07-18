c753a3b7327626662e569e528375f466668dd1d0
Tarcaxoxide <loganer@vivaldi.net>
2021-07-18 14:42:25 -0400

    75.

--- Games/Ravager/Scenes/74.gd
+++ Games/Ravager/Scenes/74.gd
@@ -1,13 +1,17 @@
 extends "res://Assets/Level.gd"
 
 export var Data:Dictionary = TemplateData.duplicate(true)
+var uame:String
 
 func _ready():
-	var uame:String=Get_Stat_Value("PlayerName",Data)
-	$Dialog_Output.text="Adeline:\"Ted,\""
+	uame=str(Get_Stat_Value("PlayerName",Data))
+	$Dialog_Output.text="Adeline:\""+uame+",\""
 	Data["Level"]["Current Scene"]="74"
 	Data["Level"]["Options"].append(_NewOption("Continue.","res://Games/Ravager/Scenes/75.tscn",$Selection))
 	_prep_ready(Data)
 
 func _process(delta):
+	if(uame == "Null"):
+		uame=str(Get_Stat_Value("PlayerName",Data))
+	$Dialog_Output.text="Adeline:\""+uame+",\""
 	P_process(delta,Data)
--- Games/Ravager/Scenes/75.gd
+++ Games/Ravager/Scenes/75.gd
@@ -3,9 +3,9 @@ extends "res://Assets/Level.gd"
 export var Data:Dictionary = TemplateData.duplicate(true)
 
 func _ready():
-	$Dialog_Output.text=""
-	Data["Level"]["Current Scene"]="0"
-	#Data["Level"]["Options"].append(_NewOption("Continue.","res://Games/Ravager/Scenes/1.tscn",$Selection))
+	$Dialog_Output.text="Adeline:\"show him what he is dealing with.\""
+	Data["Level"]["Current Scene"]="75"
+	Data["Level"]["Options"].append(_NewOption("Continue.","res://Games/Ravager/Scenes/wip.tscn",$Selection))
 	_prep_ready(Data)
 
 func _process(delta):
